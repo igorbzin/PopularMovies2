1cec2ebbf98f8d6df270eafdd9598d0e3fb2fc0d
Igor Bozin <igorbzin@gmail.com>
2017-05-13 16:34:36 +0200

    update

--- app/src/main/java/org/bozin/igor/popularmovies/MainActivity.java
+++ app/src/main/java/org/bozin/igor/popularmovies/MainActivity.java
@@ -1,45 +1,45 @@
 package org.bozin.igor.popularmovies;
 
-import android.content.res.Resources;
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
+
+
+        import android.content.res.Resources;
+        import android.os.AsyncTask;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.support.v7.widget.GridLayoutManager;
+        import android.support.v7.widget.RecyclerView;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ArrayAdapter;
+        import android.widget.Spinner;
+
+        import java.net.URL;
+        import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private GridLayoutManager gridLayoutManager;
     private MovieAdapter movieAdapter;
-    private String sortBy;
+    private String sortByPopular ;
+    private String sortByRating ;
     private Spinner dropdown;
     private String random;
     private String popular;
     private String rating;
     public static Resources resources;
-    public ArrayList<Movie> movieList;
 
 
-    public void setMovieList(ArrayList<Movie> movieList) {
-        this.movieList = movieList;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sortByPopular = getString(R.string.sort_by_popular);
+        sortByRating= getString(R.string.sort_by_rating);
         resources = getResources();
-        movieList = new ArrayList<Movie>();
         random = getString(R.string.random);
         popular = getString(R.string.popular);
         rating = getString(R.string.rating);
@@ -48,47 +48,43 @@ public class MainActivity extends AppCompatActivity {
         dropdown = (Spinner) findViewById(R.id.spinner_dropdown);
         ArrayAdapter<String> dropDownAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(dropDownAdapter);
-
         gridLayoutManager = new GridLayoutManager(MainActivity.this, 2);
         recyclerView.setLayoutManager(gridLayoutManager);
-        movieAdapter = new MovieAdapter(movieList);
-        recyclerView.setAdapter(movieAdapter);
-        sortBy = resources.getString(R.string.sort_by_popular);
-        loadMovieData(sortBy);
-        movieAdapter = new MovieAdapter(movieList);
-        recyclerView.setAdapter(movieAdapter);
+
+        loadMovieData(sortByPopular);
+
+
 
         dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            int lastSelectedItem = -1;
+
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        sortBy = getString(R.string.sort_by_popular);
-                        loadMovieData(sortBy);
-                        movieAdapter = new MovieAdapter(movieList);
-                        recyclerView.setAdapter(movieAdapter);
-                        Log.v("Dropdown", "random got selected");
-                        return;
-                    case 1:
-                        sortBy = getString(R.string.sort_by_popular);
-                        loadMovieData(sortBy);
-                        movieAdapter = new MovieAdapter(movieList);
-                        recyclerView.setAdapter(movieAdapter);
-                        Log.v("Dropdown", "popular got selected");
-                        return;
-                    case 2:
-                        sortBy = getString(R.string.sort_by_rating);
-                        loadMovieData(sortBy);
-                        movieAdapter = new MovieAdapter(movieList);
-                        recyclerView.setAdapter(movieAdapter);
-                        Log.v("Dropdown", "rating got selected");
-                        return;
+                String message = "";
+                if (position != lastSelectedItem  && position > 0 && position < 3) {
+                    switch (position) {
+                        case 0:
+                            loadMovieData(sortByPopular);
+                            message = "random got selected";
+                            break;
+                        case 1:
+                            loadMovieData(sortByPopular);
+                            message = "popular got selected";
+                            break;
+                        case 2:
+                            loadMovieData(sortByRating);
+                            message = "rating got selected";
+                            break;
+                    }
+                    Log.v("Dropdown", message);
+                    lastSelectedItem = position;
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
+//maybe some check here, fill some list or whatever what prepares some data
             }
         });
 
@@ -99,24 +95,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public class FetchMoviesTask extends AsyncTask<String, Void, ArrayList<Movie>>{
-
-        private ArrayList<Movie> movieArray;
-
         @Override
         protected ArrayList<Movie> doInBackground(String... params) {
-            if(params.length == 0){
-                return null;
-            }
+            ArrayList<Movie> movieArray = null;
 
-            String sortCriterium = params[0];
-            URL movieRequestURL = NetworkUtils.buildURL(sortCriterium);
-            movieArray = new ArrayList<Movie>();
-            try{
-                String JSONMovieResponse = NetworkUtils.getResponseFromHttpURL(movieRequestURL);
-                movieArray = PopularMoviesJSONUtils.getMoviesFromJSON(MainActivity.this, JSONMovieResponse);
-            }
-            catch (Exception e){
-                e.printStackTrace();
+            if(params != null && params.length > 0){
+                String sortCriterium = params[0];
+                URL movieRequestURL = NetworkUtils.buildURL(sortCriterium);
+                movieArray = new ArrayList<Movie>();
+
+                try {
+                    String JSONMovieResponse = NetworkUtils.getResponseFromHttpURL(movieRequestURL);
+                    movieArray = PopularMoviesJSONUtils.getMoviesFromJSON(MainActivity.this, JSONMovieResponse);
+
+                } catch (Exception e){  // try here to distinguish between exceptions
+                    e.printStackTrace();
+                }
             }
             return movieArray;
         }
@@ -124,8 +118,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(ArrayList<Movie> movieArray) {
             if(movieArray!= null){
-               setMovieList(movieArray);
+
+                movieAdapter = new MovieAdapter(movieArray);
+                recyclerView.setAdapter(movieAdapter);
             }
-            }
+        }
     }
 }
